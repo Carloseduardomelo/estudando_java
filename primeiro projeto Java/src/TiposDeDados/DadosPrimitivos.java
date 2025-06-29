8fa3eccca1f3e1be26f816e4f44c28acc7aebb6d
carlos <edu464377@gmail.com>
2025-06-29 12:08:23 -0300

    primeira sobre dados primitivos

---
+++ primeiro projeto Java/src/TiposDeDados/DadosPrimitivos.java
@@ -0,0 +1,21 @@
+package TiposDeDados;
+
+public class DadosPrimitivos {
+    public static void main(String[] args) {
+        /*
+        * Tipos de dados: int, ch
+        ar , float , boolean , double
+        * Objetivo criar um ninja
+        * */
+
+        String name = "naruto";
+        int Idade = 18;
+        double altura = 1.80;
+        boolean VivoOuMorto = true;
+
+        System.out.println(name);
+        System.out.println(Idade);
+        System.out.println(altura);
+        System.out.println(VivoOuMorto);
+    }
+}
