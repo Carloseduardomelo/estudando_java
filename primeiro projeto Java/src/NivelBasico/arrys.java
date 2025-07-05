be91eb3f8742346c6b5d85e776959af9cbcaf24a
carlos <edu464377@gmail.com>
2025-07-05 19:58:13 -0300

    Estutando arrys

---
+++ primeiro projeto Java/src/NivelBasico/arrys.java
@@ -0,0 +1,18 @@
+package NivelBasico;
+
+public class arrys {
+    public static void main(String[] args) {
+        /*
+        * Objetivo estudar os arrys e como eles funciona.
+        * */
+
+        String[] namesNinja = new String[3]; // podemos faazer um qualquer tipo.
+
+        namesNinja[0] = "Naruto";
+        namesNinja[1] = "Sasuke";
+        namesNinja[2] = "Hinata";
+
+        System.out.println(namesNinja[0]);
+
+    }
+}
