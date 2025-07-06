f1b3303105bcd055e84307166d4dc9a85901efae
carlos <edu464377@gmail.com>
2025-07-06 15:33:38 -0300

    Criandos novos metodos para a class ninja

--- Curso java 10X/src/NivelINtermetiario/Ninja.java
+++ Curso java 10X/src/NivelINtermetiario/Ninja.java
@@ -4,4 +4,20 @@ public class Ninja {
     String Name;
     String Aldeia;
     int Idade;
+
+    // o Void ele não retorna nada, ja os outro retornam alguma coisa.
+
+    public void CharinganAtividado (){
+        System.out.println("o Charingan foi ativado");
+    }
+
+    public String EuSouUmNinja() {
+        return  "Ola eu sou um ninja"; //sempre temos que colocar esse palavra para retorna alguam cois.
+    }
+
+    public int QuantoTempoFaltaParaSerHocage (int Idademinina){
+        return Idademinina - Idade;
+    }
+
+
 }
