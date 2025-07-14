d3f3119a39122cb7474429c05a1b367a6de6e435
Carloseduardomelo <127193594+Carloseduardomelo@users.noreply.github.com>
2025-07-14 15:17:15 -0300

    estudando construtores

---
+++ Curso java 10X/src/construdor/main.java
@@ -0,0 +1,9 @@
+package construdor;
+
+public class main {
+    public static void main(String[] args) {
+        rokage ninja01 = new rokage("tobirama", 200, true);
+
+        System.out.println(ninja01.name);
+    }
+}
---
+++ Curso java 10X/src/construdor/rokage.java
@@ -0,0 +1,18 @@
+package construdor;
+
+public class rokage {
+    String name;
+    boolean vivo;
+    int idade;
+
+    // construtur no args
+    public rokage () {
+
+    }
+    // construtor all args
+    public  rokage (String name,int idade , boolean vivo){
+        this.name = name;
+        this.idade = idade;
+        this.vivo = vivo;
+    }
+}
