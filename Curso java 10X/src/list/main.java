74703615a8cd94d7220455459d88e3f217e8cc45
Carlos eduardo <edu464377@gmail.com>
2025-07-23 11:12:14 -0300

    estutando LIST

---
+++ Curso java 10X/src/list/main.java
@@ -0,0 +1,26 @@
+package list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class main {
+    public static void main(String[] args) {
+
+        //Arrys
+        // Eles são estaticos, e não dar para mecher lenes
+        String[] ninjasArrys = new String[3];
+        ninjasArrys[0] = "naruto";
+        ninjasArrys[1] = "sasuque";
+        ninjasArrys[2] = "sakura";
+        System.out.println("montrando a referencia de menoria do Arry = " + ninjasArrys);
+
+        //List
+        List<String> ninjasList = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            ninjasList.add("oi");
+        }
+        System.out.println(ninjasList);
+
+
+    }
+}
