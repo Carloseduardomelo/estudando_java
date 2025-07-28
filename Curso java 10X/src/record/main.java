0156622b078ca279f97584eafca08190b57d8eb3
carlos eduardo <edu464377@gmail.com>
2025-07-28 20:52:03 -0300

    Estudando record

---
+++ Curso java 10X/src/record/main.java
@@ -0,0 +1,10 @@
+package record;
+
+public class main {
+    public static void main(String[] args) {
+
+        ninjaRecord ninja = new ninjaRecord("naruto" , 14);
+        System.out.println(ninja);
+
+    }
+}
---
+++ Curso java 10X/src/record/ninjaRecord.java
@@ -0,0 +1,6 @@
+package record;
+
+public record ninjaRecord(String name, int idade) {
+    // Todos os atributos do record são final.
+    // Os record nãp tem setter.
+}
